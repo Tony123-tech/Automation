24c16b6a41a1df491af19b98e917bb026a191efb
Tony <tony1225leung@gmail.com>
2026-08-21 17:33:53 +0800

    Add captcha bot automation script

---
+++ Neal.fun/I-am-not-a-robot/neal.js
@@ -0,0 +1,21 @@
+function runCaptchaBot() {
+    let level1Box = document.querySelector('.checkmark-spinner-container') || document.querySelector('.checkbox');
+    if (level1Box) {
+        level1Box.click();
+        setTimeout(runCaptchaBot, 5000);
+        return;
+    }
+
+    let gridItems = document.querySelectorAll('.grid-item');
+    if (gridItems.length > 0) {
+        let targets = [2, 3, 6, 7]; 
+        targets.forEach(index => {
+            if (gridItems[index]) gridItems[index].click();
+        });
+
+        setTimeout(() => {
+            document.querySelector('#captcha-verify-button')?.click();
+            setTimeout(runCaptchaBot, 5000);
+        }, 5000);
+    }
+}
